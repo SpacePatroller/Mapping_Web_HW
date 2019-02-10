243d4b7baabc56e37177089fee27f9dce5596410
SpacePatroller <emanshupatel@emanshus-mbp.attlocal.net>
2019-02-10 12:32:18 -0600

    need to work on legend

--- logic.js
+++ logic.js
@@ -1,16 +1,70 @@
 
-var mymap = L.map('mapped').setView([34.0522, -118.2437], 8);
+var mymap = L.map('mapped').setView([49.975514, -153.006376], 5);
 
 // tile layer
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    zoom: 5,
+    zoom: 50,
     id: "mapbox.streets",
     accessToken: API_KEY
 }).addTo(mymap);
 
+var legend = L.control();
 
+legend.onAdd = function (mymap) {
 
+     
+    underOnePointNine = '#426eff',
+    threePointNine = "#9ed3e5",
+    fourPointNine = "#e7f252",
+    fivePointNine = "#ffa551",
+    sixPointNine = "#d051ff",
+    sevenPointNine = "#ff30e0",
+    eightPointNine = "#ff3052",
+    ninePoint = "#ff0000"
+
+var getColor = function (mags) {
+    if (mags <= 1) {
+        return underOnePointNine
+    }
+    else if (mags <= 1.5) {
+        return threePointNine
+    }
+    else if (mags <= 2.0) {
+        return fourPointNine
+    }
+    else if (mags <= 2.5) {
+        return fivePointNine
+    }
+    else if (mags <= 3.0) {
+        return sixPointNine
+    }
+    else if (mags <= 3.5) {
+        return sevenPointNine
+    }
+    else if (mags <= 4.0) {
+        return eightPointNine
+    }
+    else if (mags > 4.0) {
+        return ninePoint
+    }
+}
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0,1,1.5,2.0,2.5,3.0,3.5,4],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(mymap);
 
 var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson';
 d3.json(url).then(function (data) {
@@ -79,69 +133,12 @@ d3.json(url).then(function (data) {
             fillOpacity: fillOppacity(mag),
             radius: magRadius(mag)
         }).addTo(mymap);
-
     }
-
-
-
 })
 
-underOnePointNine = '#426eff',
-    threePointNine = "#9ed3e5",
-    fourPointNine = "#e7f252",
-    fivePointNine = "#ffa551",
-    sixPointNine = "#d051ff",
-    sevenPointNine = "#ff30e0",
-    eightPointNine = "#ff3052",
-    ninePoint = "#ff0000"
-
-var magColor = function (mags) {
-    if (mags <= 1) {
-        return underOnePointNine
-    }
-    else if (mags <= 1.5) {
-        return threePointNine
-    }
-    else if (mags <= 2.0) {
-        return fourPointNine
-    }
-    else if (mags <= 2.5) {
-        return fivePointNine
-    }
-    else if (mags <= 3.0) {
-        return sixPointNine
-    }
-    else if (mags <= 3.5) {
-        return sevenPointNine
-    }
-    else if (mags <= 4.0) {
-        return eightPointNine
-    }
-    else if (mags > 4.1) {
-        return ninePoint
-    }
-}
 
 
-var legend = L.control({ position: 'bottomright' });
 
-legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-        labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + magColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-
-    return div;
-};
-
-legend.addTo(mymap);
 
 
 
